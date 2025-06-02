1e837dd04168684e128ffdf603eedfd3ad82baba
LelouchViBritannia2 <duobletriple@gmail.com>
2025-06-02 17:44:02 +1000

    Add files via upload

--- project heartstone/Main.py
+++ project heartstone/Main.py
@@ -6,20 +6,20 @@ WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("project HeartStone")
 running = True
-
-#окно игры
-surf = pygame.Surface((100,200))
-surf.fill('orange')
-x = 100
-y = 150
+clock =  pygame.time.Clock()
+#окно карт
+cadr_sufr = pygame.Surface((120,200))
+card_rect = cadr_sufr.get_frect(center = (WINDOW_WIDTH/2, WINDOW_HEIGHT - 100))
+cadr_sufr.fill('orange')
 
 #запуск и ход игры
 while running:
+    clock.tick()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     display_surface.fill("darkgray")
-    display_surface.blit(surf,(x,y))
+    display_surface.blit(cadr_sufr,card_rect)
     pygame.display.update()
 
 pygame.quit()
